1eb9016a388cc281fe1fa7c61f05e3aef82acfa9
Edy Espinal <edyespinal@gmail.com>
2022-03-28 22:04:25 +0200

    chore: remove temporary redirect

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { Container, Text, useMantineTheme } from '@mantine/core'
+import { Container, Text, Image, useMantineTheme } from '@mantine/core'
 import type { NextPage } from 'next'
 
 import { SvLogo } from 'components/icons/VsLogo'
@@ -6,9 +6,9 @@ import { Layout } from 'components/layout/Layout'
 import { useWindowSize } from 'hooks/useWindowSize'
 import { useStyles } from 'styles/save-the-date'
 
-const Home: NextPage = () => {
+const SaveTheDate: NextPage = () => {
   const { classes } = useStyles()
-  const { breakpoints } = useMantineTheme()
+  const { breakpoints, colors } = useMantineTheme()
   const [width] = useWindowSize()
   let size: Size = 'md'
 
@@ -17,16 +17,28 @@ const Home: NextPage = () => {
   }
 
   return (
-    <Layout>
+    <Layout color={colors.blue[0]}>
       <Container className={classes.wrapper}>
-        <Container>
-          <SvLogo size={size} />
-          <Text>VICTOR &amp; STEFANIE</Text>
-          <Text className={classes.date}>13.08.22 | Honduras</Text>
-        </Container>
+        <div className={classes.container}>
+          <Container className={classes.image}>
+            <Image
+              src="/img/foto.jpg"
+              alt="Víctor &amp; Stefanie"
+              width="100%"
+            />
+            <Text className={classes.imageText}>Save the Date</Text>
+          </Container>
+          <Container className={classes.logo}>
+            <Container>
+              <SvLogo size={size} />
+              <Text>VICTOR &amp; STEFANIE</Text>
+              <Text className={classes.date}>13.08.22 | Honduras</Text>
+            </Container>
+          </Container>
+        </div>
       </Container>
     </Layout>
   )
 }
 
-export default Home
+export default SaveTheDate
